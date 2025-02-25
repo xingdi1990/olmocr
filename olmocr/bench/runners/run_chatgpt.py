aac0c1503da3f59c4eb176691cc2fc2e7cb68c39
Jake Poznanski <jakep@allenai.org>
2025-02-25 13:46:36 -0800

    chatgpt converter

--- olmocr/bench/runners/run_chatgpt.py
+++ olmocr/bench/runners/run_chatgpt.py
@@ -1,14 +1,14 @@
-# type: ignore
 import os
+import json
 
 from openai import OpenAI
 
-from olmocr.data.anchor import get_anchor_text
-from olmocr.data.prompts import build_openai_silver_data_prompt
+from olmocr.prompts.anchor import get_anchor_text
+from olmocr.prompts.prompts import build_openai_silver_data_prompt, openai_response_format_schema, PageResponse
 from olmocr.data.renderpdf import render_pdf_to_base64png
 
 
-def run_chatgpt(pdf_path: str, page_num: int = 1, model: str = "gpt-4o-2024-08-06") -> str:
+def run_chatgpt(pdf_path: str, page_num: int = 1, model: str = "gpt-4o-2024-08-06", temperature: float=0.1) -> str:
     """
     Convert page of a PDF file to markdown using GOT-OCR.
 
@@ -22,8 +22,8 @@ def run_chatgpt(pdf_path: str, page_num: int = 1, model: str = "gpt-4o-2024-08-0
         str: The OCR result in markdown format.
     """
     # Convert the first page of the PDF to a base64-encoded PNG image.
-    # base64image = render_pdf_to_base64png(pdf_path, page_num=page_num, target_longest_image_dim=1024)
-    anchor_text = get_anchor_text(local_pdf_path, page, pdf_engine="pdfreport")
+    image_base64 = render_pdf_to_base64png(pdf_path, page_num=page_num, target_longest_image_dim=2048)
+    anchor_text = get_anchor_text(pdf_path, page_num, pdf_engine="pdfreport")
 
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -38,12 +38,18 @@ def run_chatgpt(pdf_path: str, page_num: int = 1, model: str = "gpt-4o-2024-08-0
                 ],
             }
         ],
-        temperature=0.1,
+        temperature=temperature,
         max_tokens=3000,
-        logprobs=True,
-        top_logprobs=5,
         response_format=openai_response_format_schema(),
     )
-    print(response)
 
-    return result
+    raw_response = response.choices[0].message.content
+
+    assert len(response.choices) > 0
+    assert response.choices[0].message.refusal is None
+    assert response.choices[0].finish_reason == "stop"
+
+    data = json.loads(raw_response)
+    data = PageResponse(**data)
+
+    return data.natural_text
