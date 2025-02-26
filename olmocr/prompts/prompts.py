98f376630ae0ee99591acd0c84f83b0a95fc7436
“aman-17” <amanrangapur@gmail.com>
2025-02-26 13:36:20 -0800

    restored the fine-tuning prompt

--- olmocr/prompts/prompts.py
+++ olmocr/prompts/prompts.py
@@ -227,7 +227,7 @@ def build_find_difference_prompt(base_text: str) -> str:
 # It's simplified from the prompt which was used to generate the silver data, and can change from dataset to dataset
 def build_finetuning_prompt(base_text: str) -> str:
     return (
-        f"Below is the image of one page of a document, as well as some raw textual content that was previously extracted for it."
+        f"Below is the image of one page of a document, as well as some raw textual content that was previously extracted for it. "
         f"Just return the plain text representation of this document as if you were reading it naturally.\n"
         f"Do not hallucinate.\n"
         f"RAW_TEXT_START\n{base_text}\nRAW_TEXT_END"
