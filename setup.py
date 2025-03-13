671e4a7dae5b03941dcf47c7c0ccabe7380ea926
Chris Wilhelm <chrisw@allenai.org>
2025-03-13 13:53:59 -0700

    move the gpu reqs to setup.py altogether

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
 setup(
+    extras_require={
+        "gpu": [
+            "sgl-kernel==0.0.3.post1",
+            "sglang[all]==0.4.2",
+            "flashinfer"
+        ],
+    },
     dependency_links=["https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/"],
 )
